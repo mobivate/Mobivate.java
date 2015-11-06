eda268d1a6ec009151dff30585bbd56117bb0be2
mildlettuce <mildlettuce@users.noreply.github.com>
2015-11-06 11:02:09 +1100

    delivered receipt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -35,13 +35,13 @@ public class Main {
 
         a.sendBatchToMulti("origin", routeId);
         */
-        
+
         String xml = "<deliverymessage><created>2011-07-11T11:30:36+0000</created><id>0ed7b241-096e-4ca4-abc3-a365f11fae8f</id><body>Test 11:33</body><inReplyTo>cad1bf26-1294-4076-8b44-1197f2568104</inReplyTo><logicalMessageId>c8aca89b-84e1-42fe-a32c-5050cd6a0590</logicalMessageId><originator>44000009999</originator><recipient>44000000001</recipient></deliverymessage>";
         DeliveryMessage deliveryMessage = a.receiveMessage(xml);
         System.out.println("Received message " + deliveryMessage);
 
 
-        xml = "<deliveryreceipt><created>2000-12-19T06:29:56+0000</created><deliveryMessageId>2ae13f9d-f5dc-4478-ab17-ba13ddeffad2</deliveryMessageId><status>ACCEPTED</status><clientReference>myref</clientReference><part>1</part><parts>1</parts></deliveryreceipt>";
+        xml = "<deliveryreceipt><created>2000-12-19T06:29:56+0000</created><deliveryMessageId>2ae13f9d-f5dc-4478-ab17-ba13ddeffad2</deliveryMessageId><status>DELIVERED</status><clientReference>myref</clientReference><part>1</part><parts>1</parts></deliveryreceipt>";
         DeliveryReceipt deliveryReceipt = a.receiveReceipt(xml);
         System.out.println("Received receipt " + deliveryReceipt);
     }
